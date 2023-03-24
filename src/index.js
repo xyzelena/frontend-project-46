d2cbb1ae831f3b64e6adce2516aa9895e4ff576f
xyzelena <emkhuzhina@gmail.com>
2023-03-25 02:51:20 +0530

    Fix with lint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import readFile from './readFile.js';
+import compareFiles from './compareFiles.js';
 
 const gendiff = (filepath1, filepath2) => {
   const file1 = readFile(filepath1);
-  console.log(file1);  
+  const file2 = readFile(filepath2);
+
+  return compareFiles(file1, file2);
 };
 
-//gendiff('file1.json', 'file2.json'); 
+// gendiff('file1.json', 'file2.json');
 
-export default gendiff; 
+export default gendiff;
--- src/readFile.js
+++ src/readFile.js
@@ -6,10 +6,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-     
+
 const readFile = (filename) => {
   const data = fs.readFileSync(getFixturePath(filename), 'utf-8');
   return JSON.parse(data);
 };
 
-export default readFile; 
+export default readFile;
