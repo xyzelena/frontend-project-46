b73d4c86c55f9ea985f2343aed9cf3adf71bb258
xyzelena <emkhuzhina@gmail.com>
2023-03-25 02:41:24 +0530

    Add func to read path and file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-const gendiff = (filepath1, filepath2) => {
+import readFile from './readFile.js';
 
+const gendiff = (filepath1, filepath2) => {
+  const file1 = readFile(filepath1);
+  console.log(file1);  
 };
+
+//gendiff('file1.json', 'file2.json'); 
 
 export default gendiff; 
---
+++ src/readFile.js
@@ -0,0 +1,15 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import fs from 'fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+     
+const readFile = (filename) => {
+  const data = fs.readFileSync(getFixturePath(filename), 'utf-8');
+  return JSON.parse(data);
+};
+
+export default readFile; 
