b224e3155b4e26b2905c657c2336c7b3bcf463d2
xyzelena <emkhuzhina@gmail.com>
2023-03-29 02:31:03 +0530

    Change name filename to file

--- src/readFile.js
+++ src/readFile.js
@@ -6,21 +6,20 @@ import yaml from 'js-yaml';
 const parseFile = (data, ext) => {
   let parse;
   if (ext === '' || ext === '.json') parse = JSON.parse;
-  else if (ext === '.yml' || ext === '.yaml') parse = yaml.load; 
+  else if (ext === '.yml' || ext === '.yaml') parse = yaml.load;
 
   return parse(data);
 };
 
+const readFile = (file) => {
+  const ext = path.extname(file);
 
-const readFile = (filename) => {
-  const ext = path.extname(filename);  
-  
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
 
-  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+  const getFixturePath = (file) => path.join(__dirname, '..', '__fixtures__', file);
 
-  const data = fs.readFileSync(getFixturePath(filename), 'utf-8');
+  const data = fs.readFileSync(getFixturePath(file), 'utf-8');
 
   return parseFile(data, ext);
 };
