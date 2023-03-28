0d393c64e9e9f22fe2d84bae9819e10f0e578cb8
xyzelena <emkhuzhina@gmail.com>
2023-03-28 18:08:48 +0530

    Add  @jest/globals

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,15 +1,15 @@
+import { test, expect } from '@jest/globals';
 import gendiff from '../src/index.js';
 
-const result = 
-`{
+const result = `{
  - follow: false
    host: hexlet.io
  - proxy: 123.234.53.22
  - timeout: 50 
  + timeout: 20
  + verbose: true
-}`; 
+}`;
 
 test('gendiff', () => {
- expect(gendiff('file1.json', 'file2.json')).toEqual(result);
+  expect(gendiff('file1.json', 'file2.json')).toEqual(result);
 });
