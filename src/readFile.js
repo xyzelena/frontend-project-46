98eb10937126cb5492a6c2a1abd92fdfcab983cd
xyzelena <emkhuzhina@gmail.com>
2023-03-29 02:18:47 +0530

    Add parse with ext

--- src/readFile.js
+++ src/readFile.js
@@ -1,15 +1,28 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
+import yaml from 'js-yaml';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const parseFile = (data, ext) => {
+  let parse;
+  if (ext === '' || ext === '.json') parse = JSON.parse;
+  else if (ext === '.yml' || ext === '.yaml') parse = yaml.load; 
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+  return parse(data);
+};
+
 
 const readFile = (filename) => {
+  const ext = path.extname(filename);  
+  
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
   const data = fs.readFileSync(getFixturePath(filename), 'utf-8');
-  return JSON.parse(data);
+
+  return parseFile(data, ext);
 };
 
 export default readFile;
