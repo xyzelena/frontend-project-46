509dd05eb02d359225ed3203a0ca6349c0ed1e59
xyzelena <emkhuzhina@gmail.com>
2023-04-10 02:18:10 +0300

    Fix with lint

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,8 +2,8 @@ import { test, expect } from '@jest/globals';
 import gendiff from '../src/index.js';
 import { readFile } from '../src/utils.js';
 
-const resultStylishJson = readFile(result.stylish.json.txt); 
-const resultStylishYml = readFile(result.stylish.yml.txt);
+const resultStylishJson = readFile('result.stylish.json.txt');
+const resultStylishYml = readFile('result.stylish.yml.txt');
 
 test.each([
   ['file1.json', 'file2.json', resultStylishJson],
