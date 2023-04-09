af4b03de7afbf4d82b66e7a080f4f3f9ff2a1c1a
xyzelena <emkhuzhina@gmail.com>
2023-04-10 02:05:06 +0300

    Change readFile() to parsrFile()

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import readFile from './readFile.js';
+import parseFile from './parseFile.js';
 import compareFiles from './compareFiles.js';
 
 const gendiff = (filepath1, filepath2) => {
-  const file1 = readFile(filepath1);
-  const file2 = readFile(filepath2);
+  const file1 = parseFile(filepath1);
+  const file2 = parseFile(filepath2);
 
   return compareFiles(file1, file2);
 };
