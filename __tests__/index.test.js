d63e9bc1130ee21dd28ad117940b58c5a4756565
xyzelena <emkhuzhina@gmail.com>
2023-04-10 02:15:42 +0300

    Move result to files txt in fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,54 +1,13 @@
 import { test, expect } from '@jest/globals';
 import gendiff from '../src/index.js';
+import { readFile } from '../src/utils.js';
 
-const result = `{
-    common: {
-      + follow: false
-        setting1: Value 1
-      - setting2: 200
-      - setting3: true
-      + setting3: null
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-        }
-        setting6: {
-            doge: {
-              - wow: 
-              + wow: so much
-            }
-            key: value
-          + ops: vops
-        }
-    }
-    group1: {
-      - baz: bas
-      + baz: bars
-        foo: bar
-      - nest: {
-            key: value
-        }
-      + nest: str
-    }
-  - group2: {
-        abc: 12345
-        deep: {
-            id: 45
-        }
-    }
-  + group3: {
-        deep: {
-            id: {
-                number: 45
-            }
-        }
-        fee: 100500
-    }
-}`;
+const resultStylishJson = readFile(result.stylish.json.txt); 
+const resultStylishYml = readFile(result.stylish.yml.txt);
 
 test.each([
-  ['file1.json', 'file2.json', result],
-  ['file1.yml', 'file2.yml', result],
+  ['file1.json', 'file2.json', resultStylishJson],
+  ['file1.yml', 'file2.yml', resultStylishYml],
 ])('.add(%i, %i)', (a, b, expected) => {
   expect(gendiff(a, b)).toBe(expected);
 });
