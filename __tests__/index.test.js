d8410450dec50e00250e386fff3f19c34ac9eb9b
xyzelena <emkhuzhina@gmail.com>
2023-04-13 03:16:13 +0300

    Fix with lint

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,12 +2,14 @@ import { test, expect } from '@jest/globals';
 import gendiff from '../src/index.js';
 import { readFile } from '../src/utils.js';
 
-const resultStylishJson = readFile('result.stylish.json.txt');
-const resultStylishYml = readFile('result.stylish.yml.txt');
+const getString = (str) => String(str).trim();
+
+const jsonStylishResult = getString(readFile('jsonStylishResult.txt'));
+const ymlStylishResult = getString(readFile('ymlStylishResult.txt'));
 
 test.each([
-  ['file1.json', 'file2.json', resultStylishJson],
-  ['file1.yml', 'file2.yml', resultStylishYml],
-])('.add(%i, %i)', (a, b, expected) => {
-  expect(gendiff(a, b)).toBe(expected);
+  ['file1.json', 'file2.json', jsonStylishResult],
+  ['file1.yml', 'file2.yml', ymlStylishResult],
+])('get diff', (a, b, expected) => {
+  expect(getString(gendiff(a, b))).toEqual(expected);
 });
