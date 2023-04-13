5f18fd1440bf4ce12f745126853b7da4c04cce50
xyzelena <emkhuzhina@gmail.com>
2023-04-13 04:53:19 +0300

    Add new tests for format plain

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,10 +6,14 @@ const getString = (str) => String(str).trim();
 
 const jsonStylishResult = getString(readFile('jsonStylishResult.txt'));
 const ymlStylishResult = getString(readFile('ymlStylishResult.txt'));
+const jsonPlainResult = getString(readFile('jsonPlainResult.txt'));
+const ymlPlainResult = getString(readFile('ymlPlainResult.txt'));
 
 test.each([
-  ['file1.json', 'file2.json', jsonStylishResult],
-  ['file1.yml', 'file2.yml', ymlStylishResult],
-])('get diff', (a, b, expected) => {
-  expect(getString(gendiff(a, b))).toEqual(expected);
+  ['file1.json', 'file2.json', 'stylish', jsonStylishResult],
+  ['file1.yml', 'file2.yml', 'stylish', ymlStylishResult],
+  ['file1.json', 'file2.json', 'plain', jsonPlainResult],
+  ['file1.yml', 'file2.yml', 'plain', ymlPlainResult],
+])('get diff', (a, b, c, expected) => {
+  expect(getString(gendiff(a, b, c))).toEqual(expected);
 });
