12be93d460796b20b8773d5f08f1c2427f53289d
xyzelena <emkhuzhina@gmail.com>
2023-04-13 07:45:52 +0300

    Add new test for json format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,14 +6,20 @@ const getString = (str) => String(str).trim();
 
 const jsonStylishResult = getString(readFile('jsonStylishResult.txt'));
 const ymlStylishResult = getString(readFile('ymlStylishResult.txt'));
+
 const jsonPlainResult = getString(readFile('jsonPlainResult.txt'));
 const ymlPlainResult = getString(readFile('ymlPlainResult.txt'));
+
+const jsonFormatJsonResult = getString(readFile('jsonFormatJsonResult.txt'));
+const ymlFormatJsonResult = getString(readFile('ymlFormatJsonResult.txt'));
 
 test.each([
   ['file1.json', 'file2.json', 'stylish', jsonStylishResult],
   ['file1.yml', 'file2.yml', 'stylish', ymlStylishResult],
   ['file1.json', 'file2.json', 'plain', jsonPlainResult],
   ['file1.yml', 'file2.yml', 'plain', ymlPlainResult],
+  ['file1.json', 'file2.json', 'json', jsonFormatJsonResult],
+  ['file1.yml', 'file2.yml', 'json', ymlFormatJsonResult],
 ])('get diff', (a, b, c, expected) => {
   expect(getString(gendiff(a, b, c))).toEqual(expected);
 });
