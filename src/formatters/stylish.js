ffdf64f024a3b8763cba47f3204d80c5fb554af9
xyzelena <emkhuzhina@gmail.com>
2023-04-13 21:03:13 +0300

    Move doStringify()

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,4 +1,23 @@
-import { getIndent, getBracketIndent, stringify } from './utils.js';
+import _ from 'lodash';
+
+const getIndent = (depth, replacer = ' ', spacesCount = 4) => replacer.repeat((depth * spacesCount) - 2);
+
+const getBracketIndent = (depth, replacer = ' ', spacesCount = 4) => replacer.repeat((depth * spacesCount) - spacesCount);
+
+const doStringify = (carrentValue, depth = 1) => {
+  if (!_.isPlainObject(carrentValue)) return `${carrentValue}`;
+
+  const carrentIndent = getIndent(depth);
+  const bracketIndent = getBracketIndent(depth);
+
+  const arrCarValue = Object.entries(carrentValue);
+
+  const lines = arrCarValue.map(([key, val]) => `${carrentIndent}  ${key}: ${doStringify(val, depth + 1)}`);
+
+  const result = ['{', ...lines, `${bracketIndent}}`].join('\n');
+
+  return result;
+};
 
 const doStylish = (diff) => {
   const iter = (carrentValue, depth = 1) => {
@@ -11,18 +30,18 @@ const doStylish = (diff) => {
           return `${carrentIndent}  ${node.key}: ${iter(node.children, depth + 1)}`;
 
         case 'deleted':
-          return `${carrentIndent}- ${node.key}: ${stringify(node.value1, depth + 1)}`;
+          return `${carrentIndent}- ${node.key}: ${doStringify(node.value1, depth + 1)}`;
 
         case 'added':
-          return `${carrentIndent}+ ${node.key}: ${stringify(node.value2, depth + 1)}`;
+          return `${carrentIndent}+ ${node.key}: ${doStringify(node.value2, depth + 1)}`;
 
         case 'unchanged':
-          return `${carrentIndent}  ${node.key}: ${stringify(node.value1, depth + 1)}`;
+          return `${carrentIndent}  ${node.key}: ${doStringify(node.value1, depth + 1)}`;
 
         case 'changed':
           return [
-            `${carrentIndent}- ${node.key}: ${stringify(node.value1, depth + 1)}`,
-            `${carrentIndent}+ ${node.key}: ${stringify(node.value2, depth + 1)}`,
+            `${carrentIndent}- ${node.key}: ${doStringify(node.value1, depth + 1)}`,
+            `${carrentIndent}+ ${node.key}: ${doStringify(node.value2, depth + 1)}`,
           ];
 
         default:
