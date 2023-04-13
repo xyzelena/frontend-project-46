0fe3a03b2e313f57fd4b5027bb00d8a0c9108cf9
xyzelena <emkhuzhina@gmail.com>
2023-04-13 10:47:57 +0300

    Delete readFile, move readFileSync

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,18 +9,17 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filepath) => path.join(__dirname, '..', '__fixtures__', filepath);
 
-const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
-
+// const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
 const getString = (str) => String(str).trim();
 
-const jsonStylishResult = getString(readFile('jsonStylishResult.txt'));
-const ymlStylishResult = getString(readFile('ymlStylishResult.txt'));
+const jsonStylishResult = fs.readFileSync(getFixturePath('jsonStylishResult.txt'), 'utf-8');
+const ymlStylishResult = fs.readFileSync(getFixturePath('ymlStylishResult.txt'), 'utf-8');
 
-const jsonPlainResult = getString(readFile('jsonPlainResult.txt'));
-const ymlPlainResult = getString(readFile('ymlPlainResult.txt'));
+const jsonPlainResult = fs.readFileSync(getFixturePath('jsonPlainResult.txt'), 'utf-8');
+const ymlPlainResult = fs.readFileSync(getFixturePath('ymlPlainResult.txt'), 'utf-8');
 
-const jsonFormatJsonResult = getString(readFile('jsonFormatJsonResult.txt'));
-const ymlFormatJsonResult = getString(readFile('ymlFormatJsonResult.txt'));
+const jsonFormatJsonResult = fs.readFileSync(getFixturePath('jsonFormatJsonResult.txt'), 'utf-8');
+const ymlFormatJsonResult = fs.readFileSync(getFixturePath('ymlFormatJsonResult.txt'), 'utf-8');
 
 test.each([
   ['file1.json', 'file2.json', 'stylish', jsonStylishResult],
@@ -30,5 +29,5 @@ test.each([
   ['file1.json', 'file2.json', 'json', jsonFormatJsonResult],
   ['file1.yml', 'file2.yml', 'json', ymlFormatJsonResult],
 ])('get diff', (a, b, c, expected) => {
-  expect(getString(gendiff(a, b, c))).toEqual(expected);
+  expect(getString(gendiff(a, b, c))).toEqual(getString(expected));
 });
