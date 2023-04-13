0645318ec3c6c7fe7217a7a3c41b922a34e5efc5
xyzelena <emkhuzhina@gmail.com>
2023-04-13 11:38:16 +0300

    Fix path

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,9 +7,7 @@ import gendiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getFixturePath = (filepath) => path.join(__dirname, '..', '__fixtures__', filepath);
-
-// const readFile = (filepath) => fs.readFileSync(getFixturePath(filepath), 'utf-8');
+const getFixturePath = (filepath) => path.join(__dirname, filepath);
 
 const readFile = (filepath) => fs.readFileSync(path.resolve(process.cwd(), getFixturePath(filepath)), 'utf-8');
 
